86881dee205ea6bebfcf7c126b520ca38133c7bc
tmuir12 <tmuir12@gmail.com>
2014-07-07 19:11:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,6 @@ while True:
 	#need to convert float value to string to print and format it to always 4 decimal places
 	lcd.lcd_display_string("Low "+ str("{:.4f}".format(lowprice)) + " HI " + str("{:.4f}".format(highprice)), 2)
 	lcd.lcd_display_string("Current Price "+ str("{:.4f}".format(currentprice)), 3)
-	lcd.lcd_display_string("Low "+ str(lowprice) + " HI " + str(highprice), 2)
-	lcd.lcd_display_string("Current Price "+ str(currentprice), 3)
 	lcd.lcd_display_string(time.strftime("%d/%m/%Y")+" "+ time.strftime("%H:%M:%S"), 4)
 	
 	#print lines for test purposes, comment out when in use
