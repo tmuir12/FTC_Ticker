bb6bc82c733d19c74857e6745bbf742e2a395084
tmuir12 <tmuir12@gmail.com>
2014-07-07 19:10:36 +0800

    Update main.py
    Adding formatting to the high, low and current price on lines 43 and 44 to ensure LCD print is formatted currently when values with less than 4 positions to the right of the decimal space are displayed.

--- main.py
+++ main.py
@@ -39,6 +39,9 @@ while True:
 	#display price and date and time
 	#call lcd module to display on lcd
 	lcd.lcd_display_string("FTC Price in AUD", 1)
+	#need to convert float value to string to print and format it to always 4 decimal places
+	lcd.lcd_display_string("Low "+ str("{:.4f}".format(lowprice)) + " HI " + str("{:.4f}".format(highprice)), 2)
+	lcd.lcd_display_string("Current Price "+ str("{:.4f}".format(currentprice)), 3)
 	lcd.lcd_display_string("Low "+ str(lowprice) + " HI " + str(highprice), 2)
 	lcd.lcd_display_string("Current Price "+ str(currentprice), 3)
 	lcd.lcd_display_string(time.strftime("%d/%m/%Y")+" "+ time.strftime("%H:%M:%S"), 4)
